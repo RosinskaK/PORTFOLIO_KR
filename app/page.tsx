8f57cd093b978ea209550a2f5f7a547ace4c60c5
RosinskaK <rosinska.eu@gmail.com>
2024-11-17 17:11:52 +0100

    commit testing

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
         </ol>
 
         <h1>Hello Next.js !!!</h1>
+        <h1 className=" text-pink-600">Commit testing :) </h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
