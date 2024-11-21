f664975408f643106bf295d30ce918e713fbefde
RosinskaK <rosinska.eu@gmail.com>
2024-11-21 19:48:19 +0100

    adding framer motion, git icons, header component and downgrading react and readt-dom to 18.2.0 v

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,10 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50 text-slate-950 relative`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50 text-slate-950 relative h-[5000px]`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[9rem] sm:w-[68.75rem] -z-10"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[45rem] rounded-full blur-[9rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10"></div>
+
+        <Header />
         {children}
       </body>
     </html>
