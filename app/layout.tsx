4f935a8c2e2d61814a45035c4be319db1f2767c1
RosinskaK <rosinska.eu@gmail.com>
2025-01-15 18:58:03 +0100

    fixing layout bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,11 +31,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-50 text-slate-950 relative pt-28 sm:pt-36
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full bg-zinc-50 text-slate-950 relative pt-28 sm:pt-36
          dark:bg-zinc-900 dark:text-zinc-100`}
       >
-        {/* <div className="sm:bg-[#fbe2e3] absolute top-[-6rem] right-[1rem] h-[25rem] w-[31.25rem] rounded-full blur-[9rem] sm:w-[68.75rem] -z-10 dark:bg-[#993574]"></div>
-        <div className="bg-[#b373db] sm:bg-[#a6acef] absolute top-[-1rem] left-[-35rem] h-[32.25rem] w-[50rem] rounded-full blur-[9rem] sm:w-[64rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10 dark:bg-[#4268b6]"></div> */}
+        <div className="bg-[#a6acef] absolute h-[32.25rem] rounded-full blur-[9rem] -z-10 dark:bg-[#4268b6] w-[65%]"></div>
+        <div className="bg-[#eda3ed] right-0 absolute h-[20rem] rounded-full blur-[9rem] -z-20 dark:bg-[#993574] w-[35%]"></div>
         <ActiveSectionContextProvider>
           <Header />
           {children}
--- components/skills.tsx
+++ components/skills.tsx
@@ -33,7 +33,7 @@ function Skills() {
       <ul className="flex flex-wrap justify-center gap-3 text-base sm:text-lg text-zinc-800 items-center">
         {skillsData.map((skils, index) => (
           <motion.li
-            className={`sm:mb-4 mb-2 rounded-full px-3 sm:px-5 py-2 sm:py-3 bg-violet-500 dark:bg-white/10 dark:text-white/80 flex gap-2 items-center text-white shadow-lg shadow-violet-600/50`}
+            className={`sm:mb-4 mb-2 rounded-full px-3 sm:px-5 py-2 sm:py-3 bg-violet-500 dark:bg-white/10 dark:text-white/80 flex gap-2 items-center text-white shadow-lg shadow-violet-600/70`}
             key={index}
             variants={fadeInAnimationVariants}
             initial="initial"
